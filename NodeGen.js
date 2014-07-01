77714d1b090135b08aef6162324a67a118d8a837
CodeRect <coderect@gmail.com>
2014-06-30 17:48:18 -0700

    initial commit

---
+++ NodeGen.js
@@ -0,0 +1,454 @@
+var Aws = {
+	awsHandle: null,
+	
+	storageHandle: null,
+	
+	init: function() {
+		Aws.awsHandle = require('aws-sdk');
+		
+		Aws.awsHandle.config.update({
+			'accessKeyId': process.env.strAwsIdent,
+			'secretAccessKey': process.env.strAwsKey
+		});
+		
+		Aws.storageHandle = new Aws.awsHandle.S3();
+	},
+	
+	dispel: function() {
+		Aws.awsHandle = null;
+		
+		Aws.storageHandle = null;
+	}
+};
+
+var Casable = {
+	// INFO: only the id must be read in Cas2ValidationReader
+	
+	casableHandle: null,
+	
+	clientHandle: null,
+	
+	init: function() {
+		Casable.casableHandle = require('casable');
+		
+		Casable.clientHandle = Casable.casableHandle.authentication(process.env.strCasableServer, {
+			'casVersion': process.env.strCasableVersion,
+			'logoutPath': process.env.strCasableLogout
+		});
+	},
+	
+	dispel: function() {
+		Casable.casableHandle = null;
+		
+		Casable.clientHandle = null;
+	}	
+};
+
+var Express = {
+	expressHandle: null,
+	
+	compressionHandle: null,
+	
+	cookieparserHandle: null,
+	
+	multerHandle: null,
+
+	expresssessionHandle: null,
+	
+	cookiesessionHandle: null,
+
+	connectmongoHandle: null,
+	
+	serverHandle: null,
+	
+	init: function() {
+		Express.expressHandle = require('express');
+
+		Express.compressionHandle = require('compression');
+
+		Express.cookieparserHandle = require('cookie-parser');
+		
+		if (process.env.boolExpressUpload === 'true') {
+			Express.multerHandle = require('multer');
+		}
+		
+		if (process.env.strExpressSession === 'sessionCookie') {
+			Express.expresssessionHandle = require('express-session');
+			
+			Express.cookiesessionHandle = require('cookie-session');
+			
+		} else if (process.env.strExpressSession === 'sessionMongo') {
+			Express.expresssessionHandle = require('express-session');
+			
+			Express.connectmongoHandle = require('connect-mongo')(Express.expresssessionHandle);
+			
+		}
+		
+		Express.serverHandle = Express.expressHandle();
+	},
+	
+	dispel: function() {
+		Express.expressHandle = null;
+		
+		Express.compressionHandle = null;
+		
+		Express.cookieparserHandle = null;
+
+		Express.multerHandle = null;
+
+		Express.expresssessionHandle = null;
+		
+		Express.cookiesessionHandle = null;
+		
+		Express.connectmongoHandle = null;
+		
+		Express.serverHandle = null;
+	},
+	
+	run: function() {
+		Express.serverHandle.enable('trust proxy');
+		
+		Express.serverHandle.use(Express.compressionHandle({
+			'threshold': 0
+		}));
+		
+		Express.serverHandle.use(Express.cookieparserHandle());
+		
+		if (process.env.boolExpressUpload === 'true') {
+			Express.serverHandle.use(Express.multerHandle({
+				'dest': __dirname + '/tmp',
+				'limits': {
+					'fileSize': 10 * 1024 * 1024,
+					'files': 1
+				}
+			}));
+		}
+		
+		if (process.env.strExpressSession === 'sessionCookie') {
+			Express.serverHandle.use(Express.cookiesessionHandle({
+				'secret': process.env.strExpressSecret,
+				'cookie': { 
+					'maxAge': 31 * 24 * 60 * 60 * 1000
+				}
+			}));
+			
+		} else if (process.env.strExpressSession === 'sessionMongo') {
+			Express.serverHandle.use(Express.expresssessionHandle({
+				'secret': process.env.strExpressSecret,
+				'cookie': { 
+					'maxAge': 31 * 24 * 60 * 60 * 1000
+				},
+				'store': new Express.connectmongoHandle({
+					'url': process.env.strMongoServer,
+					'collection': 'collectionSession'
+				})
+			}));
+			
+		}
+		
+		Express.serverHandle.listen(process.env.intExpressPort);
+		
+		setInterval(function() {
+			var FilesystemRead_strFile = [];
+			
+			var functionFilesystemRead = function() {
+				Node.fsHandle.readdir(__dirname + '/tmp', function(errorHandle, dirHandle) {
+					if (errorHandle !== null) {
+						functionError();
+						
+						return;
+					}
+					
+					{
+						for (var intFor1 = 0; intFor1 < dirHandle.length; intFor1 += 1) {
+							FilesystemRead_strFile.push(dirHandle[intFor1]);
+						}
+					}
+					
+					functionFilesystemReadIteratorFirst();
+				});
+			};
+
+			var FilesystemReadIterator_intIndex = 0;
+			
+			var functionFilesystemReadIteratorFirst = function() {
+				{
+					var intCount = Math.min(FilesystemRead_strFile.length);
+					
+					if (FilesystemReadIterator_intIndex < intCount) {
+						functionFilesystemStat();
+						
+						return;
+					}
+				}
+				
+				functionSuccess();
+			};
+			
+			var functionFilesystemReadIteratorNext = function() {
+				{
+					FilesystemReadIterator_intIndex += 1;
+				}
+				
+				functionFilesystemReadIteratorFirst();
+			};
+			
+			var functionFilesystemStat = function() {
+				Node.fsHandle.stat(__dirname + '/tmp/' + FilesystemRead_strFile[FilesystemReadIterator_intIndex], function (errorHandle, statHandle) {
+					if (errorHandle !== null) {
+						functionError();
+						
+						return;
+					}
+					
+					if (statHandle.ctime.getTime() < new Date().getTime() - (60 * 60 * 1000)) {
+						functionFilesystemDelete();
+						
+						return;
+					}
+					
+					functionFilesystemReadIteratorNext();
+				});
+			};
+			
+			var functionFilesystemDelete = function() {
+				Node.fsHandle.unlink(__dirname + '/tmp/' + FilesystemRead_strFile[FilesystemReadIterator_intIndex], function(errorHandle) {
+					if (errorHandle !== null) {
+						functionError();
+						
+						return;
+					}
+					
+					functionFilesystemReadIteratorNext();
+				});
+			};
+			
+			var Errorsuccess_intTimestamp = new Date().getTime();
+			
+			var functionError = function() {
+				var dateHandle = new Date();
+
+				console.log('');
+				console.log('------------------------------------------------------------');
+				console.log('- Timestamp: ' + dateHandle.toISOString());
+				console.log('- Origin: NodeGen - Express');
+				console.log('- Duration: ' + (dateHandle.getTime() - Errorsuccess_intTimestamp));
+				console.log('- Status: Error');
+				console.log('------------------------------------------------------------');
+			};
+			
+			var functionSuccess = function() {
+				var dateHandle = new Date();
+
+				console.log('');
+				console.log('------------------------------------------------------------');
+				console.log('- Timestamp: ' + dateHandle.toISOString());
+				console.log('- Origin: NodeGen - Express');
+				console.log('- Duration: ' + (dateHandle.getTime() - Errorsuccess_intTimestamp));
+				console.log('- Status: Success');
+				console.log('------------------------------------------------------------');
+			};
+			
+			functionFilesystemRead();	
+		}, 5 * 60 * 1000);
+	}
+};
+
+var Hypertextminfier = {
+	hypertextminfierHandle: null,
+	
+	init: function() {
+		Hypertextminfier.hypertextminfierHandle = require('html-minifier');
+	},
+	
+	dispel: function() {
+		Hypertextminfier.hypertextminfierHandle = null;
+	}
+};
+
+var Mime = {
+	mimeHandle: null,
+	
+	init: function() {
+		Mime.mimeHandle = require('mime');
+	},
+	
+	dispel: function() {
+		Mime.mimeHandle = null;
+	}
+};
+
+var Mongo = {
+	mongoHandle: null,
+	
+	clientHandle: null,
+	
+	init: function() {
+		Mongo.mongoHandle = require('mongodb');
+		
+		Mongo.mongoHandle.MongoClient.connect(process.env.strMongoServer, function(errorHandle, clientHandle) {
+			Mongo.clientHandle = clientHandle;
+		});
+	},
+	
+	dispel: function() {
+		Mongo.mongoHandle = null;
+		
+		Mongo.clientHandle = null;
+	}
+};
+
+var Mustache = {
+	mustacheHandle: null,
+	
+	init: function() {
+		Mustache.mustacheHandle = require('mustache');
+	},
+	
+	dispel: function() {
+		Mustache.mustacheHandle = null;
+	}
+};
+
+var Phantom = {
+	// INFO: sudo apt-get install fontconfig
+	
+	phantomjsHandle: null,
+	
+	init: function() {
+		Phantom.phantomjsHandle = require('phantomjs');
+	},
+	
+	dispel: function() {
+		Phantom.phantomjsHandle = null;
+	}
+};
+
+var Recaptcha = {
+	recaptchaHandle: null,
+	
+	clientHandle: null,
+	
+	init: function() {
+		Recaptcha.recaptchaHandle = require('re-captcha');
+		
+		Recaptcha.clientHandle = new Recaptcha.recaptchaHandle(process.env.strRecaptchaPublic, process.env.strRecaptchaPrivate);
+	},
+	
+	dispel: function() {
+		Recaptcha.recaptchaHandle = null;
+		
+		Recaptcha.clientHandle = null;
+	}
+};
+
+var Socket = {
+	socketHandle: null,
+	
+	serverHandle: null,
+	
+	init: function() {
+		Socket.socketHandle = require('socket.io');
+		
+		Socket.serverHandle = Socket.socketHandle();
+	},
+	
+	dispel: function() {
+		Socket.socketHandle = null;
+		
+		Socket.serverHandle = null;
+	},
+	
+	run: function() {
+		var httpHandle = Node.httpHandle.createServer(function(requestHandle, responseHandle) {
+			responseHandle.writeHead(200, {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': 'POST, GET'
+			});
+			
+			responseHandle.end();
+		});
+		
+		{
+			Socket.serverHandle.attach(httpHandle);
+			
+			Socket.serverHandle.origins('*:*');
+		}
+		
+		httpHandle.listen(process.env.intSocketPort);
+	}
+};
+
+var Sqlite = {
+    sqliteHandle: null,
+    
+    clientHandle: null,
+    
+    init: function() {
+	    Sqlite.sqliteHandle = require('sqlite3');
+	    
+	    Sqlite.clientHandle = new Sqlite.sqliteHandle.Database(process.env.strSqliteDatabase);
+    },
+    
+    dispel: function() {
+	    Sqlite.sqliteHandle = null;
+	    
+	    Sqlite.clientHandle = null;
+    }
+};
+
+{
+	if (process.env.boolAws === 'true') {
+		Aws.init();
+	}
+	
+	if (process.env.boolCasable === 'true') {
+		Casable.init();
+	}
+	
+	if (process.env.boolExpress === 'true') {
+		Express.init();
+	}
+	
+	if (process.env.boolHypertextminfier === 'true') {
+		Hypertextminfier.init();
+	}
+	
+	if (process.env.boolMime === 'true') {
+		Mime.init();
+	}
+	
+	if (process.env.boolMongo === 'true') {
+		Mongo.init();
+	}
+	
+	if (process.env.boolMustache === 'true') {
+		Mustache.init();
+	}
+	
+	if (process.env.boolPhantom === 'true') {
+		Phantom.init();
+	}
+	
+	if (process.env.boolRecaptcha === 'true') {
+		Recaptcha.init();
+	}
+	
+	if (process.env.boolSocket === 'true') {
+		Socket.init();
+	}
+	
+	if (process.env.boolSqlite === 'true') {
+		Sqlite.init();
+	}
+}
+
+{
+	if (process.env.boolExpress === 'true') {
+		Express.run();
+	}
+	
+	if (process.env.boolSocket === 'true') {
+		Socket.run();
+	}
+}
---
+++ VoxRect.js
@@ -0,0 +1,114 @@
+var Node = {
+	childprocessHandle: null,
+	
+	cryptoHandle: null,
+	
+	fsHandle: null,
+	
+	httpHandle: null,
+	
+	pathHandle: null,
+	
+	init: function() {
+		Node.childprocessHandle = require('child_process');
+		
+		Node.cryptoHandle = require('crypto');
+		
+		Node.fsHandle = require('fs');
+
+		Node.httpHandle = require('http');
+
+		Node.pathHandle = require('path');
+	},
+	
+	dispel: function() {
+		Node.childprocessHandle = null;
+		
+		Node.cryptoHandle = null;
+		
+		Node.fsHandle = null;
+		
+		Node.httpHandle = null;
+		
+		Node.pathHandle = null;
+	}
+};
+
+{
+	Node.init();
+}
+
+{
+	with (global) {
+		eval(Node.fsHandle.readFileSync(__dirname + '/NodeGen.config').toString());
+		
+		eval(Node.fsHandle.readFileSync(__dirname + '/NodeGen.js').toString());
+	}
+}
+
+{
+	Socket.serverHandle.on('connection', function(socketHandle) {
+		socketHandle.on('disconnect', function() {
+			
+		});
+	});
+}
+
+//TODO: insert domain
+setInterval(function () {
+	var functionRequest = function() {
+		var requestHttp = Node.httpHandle.request({
+			'host': '127.0.0.1',
+			'port': 26866,
+			'path': '/host.xml?strName=' + encodeURIComponent(process.env.strName) + '&intPort=' + encodeURIComponent(process.env.intSocketPort) + '&strMotd=' + encodeURIComponent(process.env.strMotd),
+			'method': 'GET'
+		}, function(responseHttp) {
+			var strContent = '';
+			
+			responseHttp.setEncoding('UTF-8');
+			
+			responseHttp.on('data', function(strData) {
+				strContent += strData;
+			});
+			
+			responseHttp.on('end', function() {
+				functionSuccess();
+			});
+		});
+		
+		requestHttp.on('error', function(errorHandle) {
+			console.log(errorHandle);
+			functionError();
+		});
+		
+		requestHttp.end();
+	};
+	
+	var Errorsuccess_intTimestamp = new Date().getTime();
+	
+	var functionError = function() {
+		var dateHandle = new Date();
+
+		console.log('');
+		console.log('------------------------------------------------------------');
+		console.log('- Timestamp: ' + dateHandle.toISOString());
+		console.log('- Origin: VoxRect');
+		console.log('- Duration: ' + (dateHandle.getTime() - Errorsuccess_intTimestamp));
+		console.log('- Status: Error');
+		console.log('------------------------------------------------------------');
+	};
+	
+	var functionSuccess = function() {
+		var dateHandle = new Date();
+
+		console.log('');
+		console.log('------------------------------------------------------------');
+		console.log('- Timestamp: ' + dateHandle.toISOString());
+		console.log('- Origin: VoxRect');
+		console.log('- Duration: ' + (dateHandle.getTime() - Errorsuccess_intTimestamp));
+		console.log('- Status: Success');
+		console.log('------------------------------------------------------------');
+	};
+	
+	functionRequest();
+}(), 5 * 60 * 1000);
